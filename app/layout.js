9fcd226cb41e242622bd03657823c14a7da5edc4
salraza1993 <salman.ahmad@nsas-tourism.com>
2024-05-13 19:39:53 +0400

    layout page updated

--- app/layout.js
+++ app/layout.js
@@ -35,13 +35,13 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" className={kanit.variable}>
-      <head>
+      <Head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
-        <Script src="/js/microsoftClarity.js" id="clarity-script" strategy="afterInteractive"></Script>
-      </head>
+      </Head>
+      <Script src="/js/microsoftClarity.js" id="clarity-script" strategy="afterInteractive"></Script>
       <body
         className={`
           ${amadeusLight.variable}
