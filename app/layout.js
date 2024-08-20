554baf9234c0dac8fec83baa8fa956164612fa53
salraza1993 <salman.ahmad@nsas-tourism.com>
2024-08-20 12:02:39 +0400

    google tag manager script added

--- app/layout.js
+++ app/layout.js
@@ -51,6 +51,17 @@ export default function RootLayout({ children }) {
           gtag('config', 'G-WJ1LF70ELL');
         `}
       </Script>
+      {/* <!-- Google Tag Manager --> */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+            var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', 'GTM-57LJWX84')
+        `}
+      </Script>
+      {/* <!-- End Google Tag Manager --> */}
       <body
         className={`
           ${amadeusLight.variable}
@@ -64,6 +75,15 @@ export default function RootLayout({ children }) {
           ${amadeusBlack.variable}
           ${amadeusBlackItalic.variable}
         `}>
+        
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-57LJWX84" height="0" width="0" style="display:none;visibility:hidden" />`,
+          }}
+        />
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
         <Header />
           {children}
         <Footer />
