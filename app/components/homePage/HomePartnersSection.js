9ebfb5676ec6e59a3431c0d786f144aac6a3d416
Salman Raza <salraza1993@gmail.com>
2026-08-24 15:53:20 +0400

    optional chaining fixed

--- app/components/homePage/HomePartnersSection.js
+++ app/components/homePage/HomePartnersSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ImageTag from '../ImageTag'
 
 function HomePartnersSection({ data }) {
-  const logoes = data?.partnersLogoes?.edges;
+  const logos = data?.partnersLogoes?.edges;
   const content = data?.partnerSectionContent;
   return (
     <section className="home-partners-section text-center section-bg-type--white">
@@ -10,11 +10,11 @@ function HomePartnersSection({ data }) {
         { content && <div className="content-block mb-4" dangerouslySetInnerHTML={{__html: content}} ></div>}
         <div className="partner-logos-wrapper gap-2 d-flex flex-wrap justify-content-center align-items-center">
           {
-            logoes.map((item, index) => {
+            logos?.map((item, index) => {
               return <div key={index} className="partner-logo ">
                 <ImageTag width={'calc(100px + 5vw)'} src={item.node.sourceUrl} alt={item.node.altText} />
               </div>
-            })        
+            })
           }
         </div>
       </div>
